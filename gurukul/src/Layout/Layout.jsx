c111e7aa2a1394a15158955f8c8c1eca4fa3ce38
prince-sharma-988 <princesharma9807908042@gmail.com>
2025-08-27 16:40:40 +0545

    Update Layout.jsx

--- gurukul/src/Layout/Layout.jsx
+++ gurukul/src/Layout/Layout.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Header from '../component/Header';
-// import Footer from '../component/Footer';
+import Footer from '../component/Footer';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <div>
       <Header />
-      <main style={{ paddingTop: '100px' }}> {/* push content below header */}
+      <main style={{ paddingTop: '100px' }}>
         <Outlet />
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
